6793f44e4217e82bd7ddd3a6417924a248a36840
ruan <ruan.marais1590@gmail.com>
2023-05-11 22:39:10 +0200

    initial build

--- bowtie2_commands.py
+++ bowtie2_commands.py
@@ -10,8 +10,9 @@ def index_reference(reference_path, reference_prefix):
 
 
 def generate_bowtie2_commands(alignment_files_dict, reference_dict, output_dir, threads):
-    bowtie2 - x reference_index - 1 reads_1.fastq - 2 reads_2.fastq - S output.sam - p 4
-
-    convert_to_bam = ['samtools', 'view', '-b', '-S', output_sam]
-    output_bam_file = os.path.join(reference_output_directory, f'{assembly[0]}.bam')
-    sort_bam = ['samtools', 'sort', output_bam_file, '-o', sorted_output_bam_file]
+    pass
+    # bowtie2 - x reference_index - 1 reads_1.fastq - 2 reads_2.fastq - S output.sam - p 4
+    #
+    # convert_to_bam = ['samtools', 'view', '-b', '-S', output_sam]
+    # output_bam_file = os.path.join(reference_output_directory, f'{assembly[0]}.bam')
+    # sort_bam = ['samtools', 'sort', output_bam_file, '-o', sorted_output_bam_file]
